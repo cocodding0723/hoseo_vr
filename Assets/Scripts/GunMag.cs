82c55d8f07a5c7ab31f20ec179cd41acc2414c63
wsxcerd <65852550+wsxcerd@users.noreply.github.com>
2021-03-28 19:16:01 +0900

    0328 GunMake

---
+++ Assets/Scripts/GunMag.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunMag : MonoBehaviour
+{
+
+    public int gunMagCount;
+    public bool isConnect;
+    public bool isGrap;
+    public Rigidbody gunMagRigid;
+    
+    void Start()
+    {
+        isGrap = false;
+        gunMagRigid = GetComponent<Rigidbody>();
+        gunMagCount = 30;
+        if(isConnect == false)
+        {
+            gunMagRigid.useGravity = true;
+        }
+        else
+        {
+            gunMagRigid.useGravity = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
