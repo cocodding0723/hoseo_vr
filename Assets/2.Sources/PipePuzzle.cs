bd6cdcb5a76b7080117e55de8330487f10802d79
Coco <cocodding@naver.com>
2021-04-09 09:04:41 +0900

    Add PipePuzzle

---
+++ Assets/2.Sources/PipePuzzle.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipePuzzle : MonoBehaviour
+{
+    public enum Rotate{
+        Up, Down, Right, Left
+    }
+
+    public PipePuzzleNode startNode;
+
+    public bool isClear;
+
+    [SerializeField]
+    LayerMask whatIsNode;
+
+    RaycastHit hit;
+
+    PipePuzzleNode currentNode = null;
+
+    void Update()
+    {
+        Debug.DrawRay(transform.position, transform.forward * 10f, Color.red);
+        if (Input.GetMouseButtonDown(0)){
+            if (Physics.Raycast(transform.position, transform.forward, out hit, 10f, whatIsNode)){
+                currentNode = hit.transform.GetComponent<PipePuzzleNode>();
+            }
+        }
+
+        if (currentNode){
+            if (Input.GetKeyDown(KeyCode.W)){
+                StartCoroutine(currentNode.SmoothRotateAndCheck(hit.normal, Rotate.Up));
+            }
+            if (Input.GetKeyDown(KeyCode.A)){
+                StartCoroutine(currentNode.SmoothRotateAndCheck(hit.normal, Rotate.Left));
+            }
+            if (Input.GetKeyDown(KeyCode.S)){
+                StartCoroutine(currentNode.SmoothRotateAndCheck(hit.normal, Rotate.Down));
+            }
+            if (Input.GetKeyDown(KeyCode.D)){
+                StartCoroutine(currentNode.SmoothRotateAndCheck(hit.normal, Rotate.Right));
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0)){
+            currentNode = null;
+        }
+    }
+
+    void CheckPuzzleClear(){
+        
+    }
+}
---
+++ Assets/2.Sources/PipePuzzleNode.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipePuzzleNode : MonoBehaviour {
+    [System.Serializable]    
+    public class RayInfo{
+        public Vector3 startPoint;
+        public Vector3 direction;
+    }
+
+    public List<RayInfo> rayInfos = new List<RayInfo>();
+
+    [HideInInspector]
+    public List<PipePuzzleNode> nodes = new List<PipePuzzleNode>();
+
+    [SerializeField]
+    private float rayDistance = 0.15f;
+    private Transform myTransform;
+    private LayerMask whatIsNode;
+
+    bool isRotate = false;
+
+    private void Start() {
+        myTransform = transform;
+        whatIsNode = gameObject.layer;
+
+        CheckNearNodes();
+    }
+
+    private void OnDestroy() {
+        myTransform = null;
+        nodes = null;
+        rayInfos = null;
+    }
+
+    void CheckNearNodes(){
+        for (int i = 0; i < rayInfos.Count; i++){
+            RaycastHit hit;
+
+            Vector3 rayStartPoint = myTransform.rotation * (rayInfos[i].startPoint);
+            Vector3 rayDirection = myTransform.rotation * rayInfos[i].direction;
+
+            rayStartPoint = myTransform.position + rayStartPoint;
+
+            if (Physics.Raycast(rayInfos[i].startPoint, rayInfos[i].direction, out hit, rayDistance, whatIsNode)){
+                PipePuzzleNode near = hit.transform.GetComponent<PipePuzzleNode>();
+                if (near){
+                    nodes.Add(near);
+                }
+            }
+        }
+    }
+
+    public IEnumerator SmoothRotateAndCheck(Vector3 normal, PipePuzzle.Rotate rotate){
+        bool flag = isRotate;
+
+        Vector3 cross = Vector3.zero;
+
+        switch(rotate){
+            case PipePuzzle.Rotate.Up:
+                cross = Vector3.Cross(normal, Vector3.up);
+                break;
+            case PipePuzzle.Rotate.Down:
+                cross = Vector3.Cross(normal, Vector3.down);
+                break;
+            case PipePuzzle.Rotate.Right:
+                cross = Vector3.down;
+                break;
+            case PipePuzzle.Rotate.Left:
+                cross = Vector3.up;
+                break;
+        }
+
+        Quaternion targetQuaternion = Quaternion.AngleAxis(90f, cross) * transform.rotation;
+
+        Debug.DrawLine(transform.position, transform.position + cross, Color.cyan, 2f);
+
+        isRotate = true;
+
+        yield return null;
+
+        while (!flag){
+            myTransform.rotation = Quaternion.Lerp(myTransform.rotation, targetQuaternion, 0.1f);
+
+            if (Quaternion.Angle(myTransform.rotation, targetQuaternion) < 0.1f){
+                myTransform.rotation = targetQuaternion;
+                flag = true;
+                isRotate = false;
+            }
+            yield return null;
+        }
+
+        CheckNearNodes();
+    }
+
+    private void OnDrawGizmos() {
+        for (int i = 0; i < rayInfos.Count; i++){
+            Vector3 rayStartPoint = transform.rotation * (rayInfos[i].startPoint);
+            Vector3 rayDirection = transform.rotation * rayInfos[i].direction;
+
+            rayStartPoint = transform.position + rayStartPoint;
+
+            Debug.DrawRay(rayStartPoint, rayDirection * rayDistance, Color.red);
+        }
+    }
+}
